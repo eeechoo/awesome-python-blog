6188ceff41840dced5612cfaec87793a0f760010
eeechoo <caiminghui369@gmail.com>
2018-05-18 12:11:46 +0800

    用户注册、登陆、登出

--- www/handlers.py
+++ www/handlers.py
@@ -1,12 +1,116 @@
-from coroweb import get
+import json, re, time, hashlib
+import logging
 
+from aiohttp import web
+from coroweb import get
 from models import User
+from config import configs
+from models import next_id
+from apis import APIValueError, APIError
+from coroweb import get, post
 
 
 @get('/')
 async def index(request):
     users = await User.findAll()
+    return 'hello index'
+
+
+# ============================================   用户注册、登陆、登出   =============================================
+
+# --------------------------------------------   用户注册   ---------------------------------------------
+@get('/register')
+def register():
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'register.html'
     }
+
+
+COOKIE_NAME = 'awesession'
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+_COOKIE_KEY = configs.session.secret
+
+
+def user2cookie(user, max_age):
+    '''
+    Generate cookie str by user.
+    '''
+    # build cookie string by: id-expires-sha1
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = await User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    # 这里通过gravater网站获得一个头像
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    await user.save()
+    # 注册成功， 设置session cookie
+    r = web.Response()
+    # 设置cookie过期时间为一天，设置httponly放置js脚本读取到cookie，放置XXS攻击
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+# --------------------------------------------   用户登陆   ---------------------------------------------
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check passwd:
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # authenticate ok, set cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+# --------------------------------------------   用户登陆   ---------------------------------------------
+# request.getHeader("Referer")用于获取来源页地址
+# HTTPRedirection 302 - HTTPFound
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
